54eaf34ad8139d7a96b8fa1a6a7d5c479f8c0035
theshine2004 <nhatminh182004@gmail.com>
2026-08-05 18:14:01 +0700

    chore: remove redundant backup folder

--- backend_local/app/core/utils.py
+++
@@ -1,7 +0,0 @@
-import secrets
-import string
-
-def generate_join_code(length: int = 6) -> str:
-    """Generate a random join code consisting of uppercase letters and digits."""
-    characters = string.ascii_uppercase + string.digits
-    return ''.join(secrets.choice(characters) for _ in range(length))
